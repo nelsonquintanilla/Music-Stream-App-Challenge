0b52c814c993d1d9644ca045b8184294963d2b96
Nelson Quintanilla <nelson.quinta@hotmail.com>
2018-09-20 09:45:53 -0600

    refactor

---
+++ app/src/main/java/com/applaudostudios/musicstreamappchallenge/ForegroundService.java
@@ -0,0 +1,74 @@
+package com.applaudostudios.musicstreamappchallenge;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+public class ForegroundService extends Service {
+    private static final String LOG_TAG = "ForegroundService";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (intent.getAction().equals(Constants.ACTION.START_FOREGROUND_ACTION)) {
+            Log.i(LOG_TAG, "Received Start Foreground Intent ");
+            Intent notificationIntent = new Intent(this, MainActivity.class);
+            notificationIntent.setAction(Constants.ACTION.MAIN_ACTION);
+            notificationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                    | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                    notificationIntent, 0);
+
+            Intent previousIntent = new Intent(this, ForegroundService.class);
+            previousIntent.setAction(Constants.ACTION.PREV_ACTION);
+            PendingIntent ppreviousIntent = PendingIntent.getService(this, 0,
+                    previousIntent, 0);
+
+            Intent playIntent = new Intent(this, ForegroundService.class);
+            playIntent.setAction(Constants.ACTION.PLAY_ACTION);
+            PendingIntent pplayIntent = PendingIntent.getService(this, 0,
+                    playIntent, 0);
+
+            Intent nextIntent = new Intent(this, ForegroundService.class);
+            nextIntent.setAction(Constants.ACTION.NEXT_ACTION);
+            PendingIntent pnextIntent = PendingIntent.getService(this, 0,
+                    nextIntent, 0);
+
+            Notification notification = new NotificationCompat.Builder(this)
+                    .setContentIntent(pendingIntent)
+                    .setOngoing(true)
+                    .addAction(android.R.drawable.ic_media_play, "Play",
+                            pplayIntent).build();
+            startForeground(Constants.NOTIFICATION_ID.FOREGROUND_SERVICE,
+                    notification);
+        } else if (intent.getAction().equals(Constants.ACTION.PLAY_ACTION)) {
+            Log.i(LOG_TAG, "Clicked Play");
+        } else if (intent.getAction().equals(
+                Constants.ACTION.STOP_FOREGROUND_ACTION)) {
+            Log.i(LOG_TAG, "Received Stop Foreground Intent");
+            stopForeground(true);
+            stopSelf();
+        }
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(LOG_TAG, "In onDestroy");
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // Used only in case of bound services.
+        return null;
+    }
+}
--- app/src/main/java/com/applaudostudios/musicstreamappchallenge/MainActivity.java
+++ app/src/main/java/com/applaudostudios/musicstreamappchallenge/MainActivity.java
@@ -1,120 +1,40 @@
 package com.applaudostudios.musicstreamappchallenge;
 
-import android.app.ProgressDialog;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
-import android.os.AsyncTask;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.ToggleButton;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
-    private ToggleButton toggleButton;
-    private boolean playPause;
-    private MediaPlayer mediaPlayer;
-    private ProgressDialog progressDialog;
-    private Boolean initialStage = true;
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView startButton = findViewById(R.id.play_button_image);
+        ImageView stopButton = findViewById(R.id.pause_button_image);
 
-        toggleButton = findViewById(R.id.audio_stream_button);
-        mediaPlayer = new MediaPlayer();
-        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        progressDialog = new ProgressDialog(this);
-        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    toggleButton.setText("Pause Streaming");
-
-                    if (initialStage) {
-                        new Player().execute("https://www.ssaurel.com/tmp/mymusic.mp3");
-                    } else {
-                        if (!mediaPlayer.isPlaying())
-                            mediaPlayer.start();
-                    }
-
-                    playPause = true;
-                } else {
-                    toggleButton.setText("Launch Streaming");
-
-                    if (mediaPlayer.isPlaying()) {
-                        mediaPlayer.pause();
-                    }
-
-                    playPause = false;
-                }
-
-            }
-        });
-
+        startButton.setOnClickListener(this);
+        stopButton.setOnClickListener(this);
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.play_button_image:
+                Intent startIntent = new Intent(MainActivity.this, ForegroundService.class);
+                startIntent.setAction(Constants.ACTION.START_FOREGROUND_ACTION);
+                startService(startIntent);
+                break;
+            case R.id.pause_button_image:
+                Intent stopIntent = new Intent(MainActivity.this, ForegroundService.class);
+                stopIntent.setAction(Constants.ACTION.STOP_FOREGROUND_ACTION);
+                startService(stopIntent);
+                break;
 
-        if (mediaPlayer != null) {
-            mediaPlayer.reset();
-            mediaPlayer.release();
-            mediaPlayer = null;
+            default:
+                break;
         }
     }
 
-    class Player extends AsyncTask<String, Void, Boolean> {
-        @Override
-        protected Boolean doInBackground(String... strings) {
-            Boolean prepared = false;
-
-            try {
-                mediaPlayer.setDataSource(strings[0]);
-                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    @Override
-                    public void onCompletion(MediaPlayer mediaPlayer) {
-                        initialStage = true;
-                        playPause = false;
-                        toggleButton.setText("Launch Streaming");
-                        mediaPlayer.stop();
-                        mediaPlayer.reset();
-                    }
-                });
-
-                mediaPlayer.prepare();
-                prepared = true;
-
-            } catch (Exception e) {
-                Log.e("MyAudioStreamingApp", e.getMessage());
-                prepared = false;
-            }
-
-            return prepared;
-        }
-
-        @Override
-        protected void onPostExecute(Boolean aBoolean) {
-            super.onPostExecute(aBoolean);
-
-            if (progressDialog.isShowing()) {
-                progressDialog.cancel();
-            }
-
-            mediaPlayer.start();
-            initialStage = false;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-            progressDialog.setMessage("Buffering...");
-            progressDialog.show();
-        }
-    }
 }

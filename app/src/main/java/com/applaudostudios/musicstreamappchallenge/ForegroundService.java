1f0d4c4fe57423633548d7b718150757cec704e6
Nelson Quintanilla <nelson.quinta@hotmail.com>
2018-09-20 12:08:22 -0600

    serviceChannel and foreground service

--- app/src/main/java/com/applaudostudios/musicstreamappchallenge/ForegroundService.java
+++ app/src/main/java/com/applaudostudios/musicstreamappchallenge/ForegroundService.java
@@ -1,74 +1,55 @@
 package com.applaudostudios.musicstreamappchallenge;
 
-import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
-import android.util.Log;
+
+import static com.applaudostudios.musicstreamappchallenge.Notification.CHANNEL_ID;
 
 public class ForegroundService extends Service {
-    private static final String LOG_TAG = "ForegroundService";
 
+    // Called the first time the service is created
     @Override
     public void onCreate() {
         super.onCreate();
     }
 
+    // Triggered when the service starts
+    // Called every time startService is called in the service
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if (intent.getAction().equals(Constants.ACTION.START_FOREGROUND_ACTION)) {
-            Log.i(LOG_TAG, "Received Start Foreground Intent ");
-            Intent notificationIntent = new Intent(this, MainActivity.class);
-            notificationIntent.setAction(Constants.ACTION.MAIN_ACTION);
-            notificationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-                    | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
-                    notificationIntent, 0);
+        String input  = intent.getStringExtra("inputExtra");
 
-            Intent previousIntent = new Intent(this, ForegroundService.class);
-            previousIntent.setAction(Constants.ACTION.PREV_ACTION);
-            PendingIntent ppreviousIntent = PendingIntent.getService(this, 0,
-                    previousIntent, 0);
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0, notificationIntent, 0);
 
-            Intent playIntent = new Intent(this, ForegroundService.class);
-            playIntent.setAction(Constants.ACTION.PLAY_ACTION);
-            PendingIntent pplayIntent = PendingIntent.getService(this, 0,
-                    playIntent, 0);
+        // Mandatory for notification in Android Oreo or higher
+        android.app.Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Example Service")
+                .setContentText(input)
+                .setSmallIcon(R.drawable.ic_android)
+                .setContentIntent(pendingIntent)
+                .build();
 
-            Intent nextIntent = new Intent(this, ForegroundService.class);
-            nextIntent.setAction(Constants.ACTION.NEXT_ACTION);
-            PendingIntent pnextIntent = PendingIntent.getService(this, 0,
-                    nextIntent, 0);
+        startForeground(1, notification);
 
-            Notification notification = new NotificationCompat.Builder(this)
-                    .setContentIntent(pendingIntent)
-                    .setOngoing(true)
-                    .addAction(android.R.drawable.ic_media_play, "Play",
-                            pplayIntent).build();
-            startForeground(Constants.NOTIFICATION_ID.FOREGROUND_SERVICE,
-                    notification);
-        } else if (intent.getAction().equals(Constants.ACTION.PLAY_ACTION)) {
-            Log.i(LOG_TAG, "Clicked Play");
-        } else if (intent.getAction().equals(
-                Constants.ACTION.STOP_FOREGROUND_ACTION)) {
-            Log.i(LOG_TAG, "Received Stop Foreground Intent");
-            stopForeground(true);
-            stopSelf();
-        }
-        return START_STICKY;
+        return START_NOT_STICKY;
     }
 
+    // Called when the service stops
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.i(LOG_TAG, "In onDestroy");
     }
 
+    // Only needed for bound bound services (this case is a started service)
+    @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        // Used only in case of bound services.
         return null;
     }
 }
--- app/src/main/java/com/applaudostudios/musicstreamappchallenge/MainActivity.java
+++ app/src/main/java/com/applaudostudios/musicstreamappchallenge/MainActivity.java
@@ -1,40 +1,56 @@
 package com.applaudostudios.musicstreamappchallenge;
 
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private String infoNotification = "blablabla";
+    private ImageView playImageView;
+    private ImageView pauseImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView startButton = findViewById(R.id.play_button_image);
-        ImageView stopButton = findViewById(R.id.pause_button_image);
 
-        startButton.setOnClickListener(this);
-        stopButton.setOnClickListener(this);
+        playImageView = findViewById(R.id.play_button_image);
+        pauseImageView = findViewById(R.id.pause_button_image);
+
+        playImageView.setOnClickListener(this);
+        pauseImageView.setOnClickListener(this);
+
+    }
+
+    public void startService() {
+        Intent serviceIntent = new Intent(this, ForegroundService.class);
+        serviceIntent.putExtra("inputExtra", infoNotification);
+
+        // To start your service while the app itself it is in the background
+        ContextCompat.startForegroundService(this, serviceIntent);
+    }
+
+    public void stopService() {
+        Intent serviceIntent = new Intent(this, ForegroundService.class);
+        stopService(serviceIntent);
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
+    public void onClick(View view) {
+        switch (view.getId()) {
             case R.id.play_button_image:
-                Intent startIntent = new Intent(MainActivity.this, ForegroundService.class);
-                startIntent.setAction(Constants.ACTION.START_FOREGROUND_ACTION);
-                startService(startIntent);
+                startService();
                 break;
             case R.id.pause_button_image:
-                Intent stopIntent = new Intent(MainActivity.this, ForegroundService.class);
-                stopIntent.setAction(Constants.ACTION.STOP_FOREGROUND_ACTION);
-                startService(stopIntent);
+                stopService();
                 break;
-
             default:
                 break;
         }
     }
+
 
 }
